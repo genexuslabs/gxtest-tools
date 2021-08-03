73c910f3e68ca53096b2a5b0510d8a633183c625
José Lamas <jlr@genexus.com>
2021-08-03 14:20:29 -0300

    Fix main program namespace

--- TestConverter/Program.cs
+++ TestConverter/Program.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 
-namespace GeneXus.GXtest.Tools.TestConverterTestConverter
+namespace GeneXus.GXtest.Tools.TestConverter
 {
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
         }
     }
 }
