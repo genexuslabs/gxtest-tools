d55f0ee4a1b6bc80ac04a5b2a03272f54fd50b8c
José Lamas <jlr@genexus.com>
2021-08-04 11:25:55 -0300

    Added Parameter
    The <IsParameterPart> element is intentionally left out because I still don't know its purpose

---
+++ TestConverter/v3/Parameter.cs
@@ -0,0 +1,19 @@
+﻿using System.Xml.Serialization;
+
+namespace GeneXus.GXtest.Tools.TestConverter.v3
+{
+    public class Parameter
+    {
+        [XmlElement("ElementName")]
+        public string ParentId { get; set; }
+
+        [XmlElement("CommandOrd")]
+        public string ParentOrder { get; set; }
+
+        [XmlElement("ParameterOrd")]
+        public string Order { get; set; }
+
+        [XmlElement("ParameterType")]
+        public string Type { get; set; }
+    }
+}
