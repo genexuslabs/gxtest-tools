ec2ccc00436859a500b93350b8b4d2709e7cf8bb
José Lamas <jlr@genexus.com>
2021-08-04 11:24:01 -0300

    ElementName in Command is actually the id of its parent (node or edge)
    And the order completes its primary key. That is, each command is identified by its order in the list of commands of a common parent Id.

--- TestConverter/v3/Command.cs
+++ TestConverter/v3/Command.cs
@@ -5,15 +5,15 @@ namespace GeneXus.GXtest.Tools.TestConverter.v3
     public class Command
     {
         [XmlElement("ElementName")]
-        public string Id { get; set; }
+        public string ParentId { get; set; }
+
+        [XmlElement("CommandOrd")]
+        public string Order { get; set; }
 
         [XmlElement("CommandType")]
         public string Type { get; set; }
 
         [XmlElement("CommandName")]
         public string Name { get; set; }
-
-        [XmlElement("CommandOrd")]
-        public string Order { get; set; }
     }
 }
