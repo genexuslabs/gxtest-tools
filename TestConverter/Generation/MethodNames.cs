658d65877120a5b5cfd56fdc9dbebe58359b603e
José Lamas <jlr@genexus.com>
2021-08-17 13:49:27 -0300

    Added support for PauseCommand

--- TestConverter/Generation/MethodNames.cs
+++ TestConverter/Generation/MethodNames.cs
@@ -6,6 +6,7 @@
         public const string Click = "Click";
         public const string ClickByID = "ClickByID";
         public const string Go = "Go";
+        public const string PauseFor = "PauseFor";
         public const string SendKeys = "SendKeys";
         public const string Type = "Type";
         public const string TypeByID = "TypeByID";
